498d9712abc9813db262189b9eef1fa36f8e9ed7
mofas <fedt1234@hotmail.com>
2012-10-31 14:56:58 +0800

    refactor => add urlHandler module

--- views/js/index.js
+++ views/js/index.js
@@ -6,14 +6,37 @@ $(function(){
 var main = (function(o){
 
 	var fingerIndexMap = ["-","0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"];
-	var $chordDiagram , $chordList , $outputLinkWrap, $outputLink ,
-		urlObj = {},
+	var $chordDiagram , $chordList , $outputLinkWrap, $outputLink ,		
 		inputMode = 0;
 
-	urlObj.origin = ""; 
-	urlObj.short = ""; 
-	urlObj.isChange = false;
+	var bindEvent = function(){
+		$chordList = $("#chordList");
+		$chordDiagram = $('.chord_diagramWrap');
+		$outputLink = $("#outputLink");
+		$chordList.on("click", ".chordItem" , function(){			
+			$(this).siblings().removeClass("selected").end().addClass("selected");			
+		});
+		$("#addButton").on("click" , o.add);
+		$("#updateButton").on("click" , o.update);
+		$("#cancelButton").on("click" , o.cancel);
+		$("#saveImageButton").on("click" , o.saveAsImage);
+		$("#outputURLButton").on("click" , o.output);
+		$("#shorturl").on("mousedown" , o.shortUrlEvent);
+	}
 
+	o.shortUrlEvent = function(){		
+		if (!$(this).is(':checked')) {
+	        $(this).trigger("change");
+	        //Short it
+	        urlHandler.shortUrl();
+	    }
+	    else{
+	    	//Reset it 
+	    	urlHandler.resetUrl();
+	    }	    
+	}	
+
+	/**
 	o.shorturlCallback = function(){
 		gapi.client.load('urlshortener', 'v1', function(){
 			var request = gapi.client.urlshortener.url.insert({
@@ -28,89 +51,9 @@ var main = (function(o){
 			});
 		});
 	}
-
-	var parseNoteByURL = function(){		
-		var param =  window.location.href.split("?")[1] , 
-			collection , strArray , intArray , diagramArray , noteInfo;
-		//chord collection
-		if(param === undefined){
-			chord_diagram.setoutputArray([0,0,0,0,0,0]);
-			chord_diagram.parseNote();
-			return;
-		}
-
-		collection = param.split("&c=").slice(1);		
-		for(var i =0; i < collection.length ;i++){
-			strArray = collection[i].split("");
-			intArray = new Array(6);
-			for(var j=0;j < strArray.length;j++){
-				intArray[j] = isNaN(parseInt(strArray[j] , 10)) ? -1 : parseInt(strArray[j] , 10);
-			}			
-			chord_diagram.setoutputArray(intArray);
-			add(intArray);
-		}
-		
-		noteInfo = param.substring(param.indexOf("note=")+5 , param.indexOf("note=")+11);
-		diagramArray = new Array(6);
-		if(noteInfo.length > 0 ){						
-			var fingerIndexString = fingerIndexMap.join("");
-			var rawArray = noteInfo.split("");
-			for(var i = 0 ; i < rawArray.length ; i++){
-				diagramArray[i] = fingerIndexString.indexOf(rawArray[i])-1;
-			}
-			chord_diagram.setoutputArray(diagramArray);
-		}
-		else{
-			chord_diagram.setoutputArray([0,0,0,0,0,0]);
-		}
-		
-		chord_diagram.parseNote();
-	}
-
-	var bindEvent = function(){
-		$chordList = $("#chordList");
-		$chordDiagram = $('.chord_diagramWrap');
-		$outputLink = $("#outputLink");
-		$chordList.on("click", ".chordItem" , function(){			
-			$(this).siblings().removeClass("selected").end().addClass("selected");			
-		});
-		$("#addButton").on("click" , o.add);
-		$("#updateButton").on("click" , o.update);
-		$("#cancelButton").on("click" , o.cancel);
-		$("#saveImageButton").on("click" , o.saveAsImage);
-		$("#outputURLButton").on("click" , o.output);
-		$("#shorturl").on("mousedown" , shortUrlEvent);
-	}
-
-	var shortUrlEvent = function(){				
-	    if (!$(this).is(':checked')) {	        
-	        $(this).trigger("change");	        
-	        if(urlObj.short.length < 1 || urlObj.isChange){
-	        	if(window.gapi === undefined){
-	        		$.getScript("https://apis.google.com/js/client.js?onload=initGoogleAPI");
-	        	}
-	        	else{
-	        		o.shorturlCallback();
-	        	}	        	
-	        }else{
-	        	$outputLink.val(urlObj.short);
-	        }
-	    }
-	    else{
-	    	$outputLink.val(urlObj.origin);
-	    }
-	}
-
-	var checkURLIsChanged = function(url){
-		if(urlObj.origin !== url){
-			urlObj.isChange = true;
-			urlObj.origin = url;
-			urlObj.short = "";
-		}
-		else{
-			urlObj.isChange = false;
-		}
-	}
+	**/
+	
+	
 	var bindHideOutputEvent = function(){
 		$chordDiagram.add($chordList).one("click" , hidenOutputEvent);
 	}
@@ -127,37 +70,30 @@ var main = (function(o){
 		var $muteButtonWrap = $(".mutebuttonWrap");
 		$outputLinkWrap = $(".outputLinkWrap");
 		chord_diagram.init($strings , $muteButtonWrap);
-		parseNoteByURL();
+
+		var $outputLink = $("#outputLink");
+		urlHandler.init($outputLink);
+		urlHandler.parseNoteByURL();
 		bindEvent();
 	}	
 
 	o.saveAsImage = function(){
 		chord_collection.outputCollectionImage();
 	}
-
-	o.output = function(){
-		var outputString = "note=";
-		var outputArray = chord_diagram.getOutputArray();
-		for(var i = 0 ,arrIndex = outputArray.length ; i < arrIndex ; i++){
-			outputString += fingerIndexMap[outputArray[i]+1];
-		}
-		//collection
-		outputString += chord_collection.outputCollectionURL();
-		var urlEnd = (window.location.href.lastIndexOf("?") > 0) ? window.location.href.lastIndexOf("?") : window.location.href.length;
-		var url = window.location.href.substring( 0 , urlEnd ) + "?" + outputString;		
-		//var link = '<a traget="_blank" href="'+url+'">'+url+'</a>';		
+	
+	o.output = function(){				
+		var url = urlHandler.output();
 		$outputLinkWrap.css({visibility: 'visible'});
-		checkURLIsChanged(url);
 		bindHideOutputEvent();
-		$outputLink.val(url);		
+		$outputLink.val(url);
+	}	
+
+	o.add = function(){				
+		var outputArray = chord_diagram.getOutputArray();
+		o.addByArray(outputArray);
 	}
 
-	o.add = function(){
-		var outputArray = chord_diagram.getOutputArray();		
-		add(outputArray);
-	}
-
-	var add = function(outputArray){		
+	o.addByArray = function(outputArray){		
 		canvas_chord_diagram.setFingerIndex(outputArray);
 		var index = chord_collection.add(canvas_chord_diagram.getCanvas() , outputArray.slice());
 		var buttonHtml = '<a class="button" href="javascript:null;" onclick="javascript:main.edit(this , '+index+');" >編輯</a>'
@@ -223,10 +159,3 @@ var main = (function(o){
 	return o;
 
 })(main || {});
-
-
-
-window.initGoogleAPI	= function(){
-	gapi.client.setApiKey('AIzaSyATKgocs2ylWRGBQiwrQIN_BEo4E6w2QGM');	
-	main.shorturlCallback();
-}
---
+++ views/js/urlHandler.js
@@ -0,0 +1,123 @@
+
+
+
+
+window.initGoogleAPI	= function(){
+	gapi.client.setApiKey('AIzaSyATKgocs2ylWRGBQiwrQIN_BEo4E6w2QGM');	
+	urlHandler.shorturlCallback();
+}
+
+
+var urlHandler = (function(o){	
+	var fingerIndexMap = ["-","0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"];
+	var $outputLink;
+
+	var originURL = "" , 
+		shortURL = "" ,  
+		URLIsChanged = false;
+
+
+	o.init = function($el1){
+		$outputLink = $el1;
+	}	
+
+	o.parseNoteByURL = function(){		
+		var param =  window.location.href.split("?")[1] , 
+			collection , strArray , intArray , diagramArray , noteInfo;
+		//chord collection
+		if(param === undefined){
+			chord_diagram.setoutputArray([0,0,0,0,0,0]);
+			chord_diagram.parseNote();
+			return;
+		}
+
+		collection = param.split("&c=").slice(1);				
+		for(var i =0; i < collection.length ;i++){
+			strArray = collection[i].split("");
+			intArray = new Array();
+			for(var j=0;j < strArray.length;j++){
+				var index = isNaN(parseInt(strArray[j] , 10)) ? -1 : parseInt(strArray[j] , 10);								
+				intArray.push(index);				
+			}										
+			chord_diagram.setoutputArray(intArray);			
+			main.addByArray(intArray);
+		}
+		
+		noteInfo = param.substring(param.indexOf("note=")+5 , param.indexOf("note=")+11);
+		diagramArray = new Array(6);
+		if(noteInfo.length > 0 ){						
+			var fingerIndexString = fingerIndexMap.join("");
+			var rawArray = noteInfo.split("");
+			for(var i = 0 ; i < rawArray.length ; i++){
+				diagramArray[i] = fingerIndexString.indexOf(rawArray[i])-1;
+			}
+			chord_diagram.setoutputArray(diagramArray);
+		}
+		else{
+			chord_diagram.setoutputArray([0,0,0,0,0,0]);
+		}
+		
+		chord_diagram.parseNote();
+	}
+
+
+	o.shortUrl = function(){			    
+        if(shortURL.length < 1 || URLIsChanged){
+        	if(window.gapi === undefined){
+        		$.getScript("https://apis.google.com/js/client.js?onload=initGoogleAPI");
+        	}
+        	else{
+        		o.shorturlCallback();
+        	}	        	
+        }else{
+        	$outputLink.val(shortURL);
+        }	    
+	}
+
+	o.resetUrl = function(){
+		$outputLink.val(originURL);
+	}
+
+	o.shorturlCallback = function(){
+		gapi.client.load('urlshortener', 'v1', function(){
+			var request = gapi.client.urlshortener.url.insert({
+				'resource': {'longUrl': originURL}
+			});
+			request.execute(function(response) {					
+				shortURL = response.id;
+				console.log(shortURL);
+				URLIsChanged = false;
+				$outputLink.val(shortURL);
+			});
+		});
+	}
+
+	o.output = function(){
+		var outputString = "note=";
+		var outputArray = chord_diagram.getOutputArray();
+		for(var i = 0 ,arrIndex = outputArray.length ; i < arrIndex ; i++){
+			outputString += fingerIndexMap[outputArray[i]+1];
+		}
+		//collection
+		outputString += chord_collection.outputCollectionURL();
+		var urlEnd = (window.location.href.lastIndexOf("?") > 0) ? window.location.href.lastIndexOf("?") : window.location.href.length;
+		var url = window.location.href.substring( 0 , urlEnd ) + "?" + outputString;
+		checkURLIsChanged(url);		
+		return url;
+	}
+
+	var checkURLIsChanged = function(url){
+		if(originURL !== url){
+			URLIsChanged = true;
+			originURL = url;
+			shortURL = "";
+		}
+		else{
+			URLIsChanged = false;
+		}
+	}
+	
+
+	return o;
+
+})(urlHandler || {});
